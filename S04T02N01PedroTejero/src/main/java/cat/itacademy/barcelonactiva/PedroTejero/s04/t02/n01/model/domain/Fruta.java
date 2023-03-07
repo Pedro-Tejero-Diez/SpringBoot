01a4b3e66195695187714be57752f55d91c03e97
pedrete3000 <pedrete3000@hotmail.com>
2023-03-07 06:07:27 +0100

    inicial

---
+++ S04T02N01PedroTejero/src/main/java/cat/itacademy/barcelonactiva/PedroTejero/s04/t02/n01/model/domain/Fruta.java
@@ -0,0 +1,56 @@
+package cat.itacademy.barcelonactiva.PedroTejero.s04.t02.n01.model.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "frutas")
+public class Fruta {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "Cantidad")
+	private int CantidadKilos;
+	
+	public Fruta(int id, String name, int cantidadKilos) {
+		this.id = id;
+		this.name = name;
+		this.CantidadKilos = cantidadKilos;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getname() {
+		return name;
+	}
+
+	public void setname(String name) {
+		this.name = name;
+	}
+
+	public int getCantidadKilos() {
+		return CantidadKilos;
+	}
+
+	public void setCantidadKilos(int cantidadKilos) {
+		CantidadKilos = cantidadKilos;
+	}
+	@Override
+	public String toString() {
+		return "Fruta id=" + id + ", name=" + name + ", Cantidad=" + CantidadKilos;
+	}
+	
+
+}

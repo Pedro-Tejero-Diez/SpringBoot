5a6047b0b620852f02afbe4c9eadd9343e242d65
pedrete3000 <pedrete3000@hotmail.com>
2023-03-15 06:58:17 +0100

    ejercicios 15 de marzo

--- S04T02N01PedroTejero/src/main/java/cat/itacademy/barcelonactiva/PedroTejero/s04/t02/n01/controllers/FrutaController.java
+++ S04T02N01PedroTejero/src/main/java/cat/itacademy/barcelonactiva/PedroTejero/s04/t02/n01/controllers/FrutaController.java
@@ -13,34 +13,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cat.itacademy.barcelonactiva.PedroTejero.s04.t02.n01.model.domain.Fruta;
 import cat.itacademy.barcelonactiva.PedroTejero.s04.t02.n01.model.repository.FrutaRepository;
 
 @RestController
-@RequestMapping("/fruta")//para que todas las path empiecen por "fruta"
+@RequestMapping("/fruta") // para que todas las path empiecen por "fruta"
 public class FrutaController {
 
-	@Autowired //inyección de la dependencia para poder usar los métodos existentes y los que creemos nosotros en frutarepository
+	@Autowired // inyección de la dependencia para poder usar los métodos existentes en JPA
+				// repository
 	FrutaRepository frutaRepository;
 
 	@GetMapping("/getAll")
-	public ResponseEntity<List<Fruta>> getAllFrutas(@RequestParam(required = false) String name) {
+	public ResponseEntity<List<Fruta>> getAllFrutas() {
 		try {
-			List<Fruta> frutas = new ArrayList<Fruta>();	
-			
-			if (name == null)
-				frutaRepository.findAll().forEach(frutas::add);
-			else
-				frutaRepository.findByName(name).forEach(frutas::add);
+			List<Fruta> frutas = new ArrayList<Fruta>();
+
+			frutaRepository.findAll().forEach(frutas::add);
 
 			if (frutas.isEmpty()) {
 				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-				
-			} else 	return new ResponseEntity<>(frutas, HttpStatus.OK);
-			
+
+			} else
+				return new ResponseEntity<>(frutas, HttpStatus.OK);
+
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -48,7 +46,7 @@ public class FrutaController {
 
 	@GetMapping("/getOne/{id}")
 	public ResponseEntity<Fruta> getOneFruta(@PathVariable("id") int id) {
-		Optional <Fruta> frutaData = frutaRepository.findById(id);
+		Optional<Fruta> frutaData = frutaRepository.findById(id);
 
 		if (frutaData.isPresent()) {
 			return new ResponseEntity<>(frutaData.get(), HttpStatus.OK);
@@ -60,8 +58,9 @@ public class FrutaController {
 	@PostMapping("/add")
 	public ResponseEntity<Fruta> createFruta(@RequestBody Fruta fruta) {
 		try {
-			//esto de abajo no funciona, tiene que ser a través de frutarepository.porque?
-			//Fruta fruta2= new Fruta(fruta.getId(), fruta.getname(), fruta.getCantidadKilos());
+			// esto de abajo no funciona, tiene que ser a través de frutarepository.porque?
+			// Fruta fruta2= new Fruta(fruta.getId(), fruta.getname(),
+			// fruta.getCantidadKilos());
 			Fruta fruta2 = frutaRepository.save(new Fruta(fruta.getId(), fruta.getname(), fruta.getCantidadKilos()));
 			return new ResponseEntity<>(fruta2, HttpStatus.CREATED);
 		} catch (Exception e) {
@@ -69,32 +68,22 @@ public class FrutaController {
 		}
 	}
 
-	@PutMapping("/update")
+	@PutMapping("/update/{id}")
 	public ResponseEntity<Fruta> updateFruta(@PathVariable("id") int id, @RequestBody Fruta fruta) {
 		Optional<Fruta> FrutaData = frutaRepository.findById(id);
-
-		if (FrutaData.isPresent()) {
-			Fruta fruta2 = FrutaData.get();
-			fruta2.setId(fruta.getId());
-			fruta2.setname(fruta.getname());
-			fruta2.setCantidadKilos(fruta.getCantidadKilos());
-			return new ResponseEntity<>(frutaRepository.save(fruta2), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		try {
+			if (FrutaData.isPresent()) {
+				Fruta fruta2 = FrutaData.get();
+				fruta2.setId(fruta.getId());
+				fruta2.setname(fruta.getname());
+				fruta2.setCantidadKilos(fruta.getCantidadKilos());
+				return new ResponseEntity<>(frutaRepository.save(fruta2), HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		/*	public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
-		Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
-
-		if (tutorialData.isPresent()) {
-			Tutorial _tutorial = tutorialData.get();
-			_tutorial.setTitle(tutorial.getTitle());
-			_tutorial.setDescription(tutorial.getDescription());
-			_tutorial.setPublished(tutorial.isPublished());
-			return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}*/
 	}
 
 	@DeleteMapping("/delete/{id}")
@@ -108,4 +97,3 @@ public class FrutaController {
 	}
 
 }
-
